30c040ee772af0b19d432b4347a909022c16af98
DongChan <pon07116@gmail.com>
2024-11-23 14:31:56 +0900

    ch10. ChatRoomController create

---
+++ src/main/java/com/ll/ch/domain/chat/chatRoom/controller/ApiV1ChatRoomController.java
@@ -0,0 +1,21 @@
+package com.ll.ch.domain.chat.chatRoom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/chat/rooms")
+public class ApiV1ChatRoomController {
+    @GetMapping
+    public List<String> getChatRooms() {
+        return List.of(
+                "room1",
+                "room2",
+                "room3"
+        );
+    }
+}
